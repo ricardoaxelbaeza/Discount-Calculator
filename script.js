ef2b59d11be962c7e9536755f5142f12f24e993e
ricardoaxelbaeza <77639742+ricardoaxelbaeza@users.noreply.github.com>
2024-07-24 19:08:07 -0700

    checks input boxes & does not allow any letter characters to be typed in

--- script.js
+++ script.js
@@ -7,14 +7,31 @@ window.onload = () => {
     const submitButton = document.getElementById("submit-form-btn");
 
     priceInput.addEventListener("input",()=>{
-        verifyInput(priceInput);
+        checkInput(priceInput);
     });
 
     percentInput.addEventListener("input",()=>{
-        verifyInput(percentInput);
+        checkInput(percentInput);
     });
 
 
-}
 
+
+ 
+    //Checks input typed (does not allow letters to be typed in)
+    function checkInput(input){
+        console.log("in the verify funct");
+        
+        let inputArr = input.value.split('');
+   
+        for(let i = 0; i < inputArr.length; i++){
+            let currentInputArrChar = inputArr.at(i).charCodeAt();
+            if(currentInputArrChar >=97 && currentInputArrChar <=122){
+                inputArr.splice(i);
+                let tempArr = inputArr.join('');
+                input.value = tempArr;
+            }
+        }
+
+    }}
     
