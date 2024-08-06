0ce724ee0ca01b5717125cb972115548498f418a
ricardobaeza <ricardoaxelbaeza@gmail.com>
2024-08-06 16:46:49 -0700

    title and discount section UI changes

--- js/percentDropDown.js
+++ js/percentDropDown.js
@@ -1,11 +1,7 @@
-
 const percentDropDownMenu = document.getElementById("percent-select");
 
-const option0 = document.getElementById("option-0");
-const option1 = document.getElementById("option-1");
-const option2 = document.getElementById("option-2");
-const option3 = document.getElementById("option-3");
-const option4 = document.getElementById("option-4");
+
+
 
 percentDropDownMenu.addEventListener('change',()=>{
     percentInput.value = percentDropDownMenu.value;
