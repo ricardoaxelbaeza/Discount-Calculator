877a7e5bb04828ce2869f56606b28680c97f52e7
ricardobaeza <ricardoaxelbaeza@gmail.com>
2024-08-07 17:01:23 -0700

    added curtain when user presses hamburger menu

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -1,6 +1,7 @@
 const hamburgerIcon = document.getElementById("hamburger-icon");
 const webPageBody = document.getElementById("calculator");
 const mobileNavMenu = document.getElementById("mobile-nav");
+const navCurtain = document.getElementById("nav-curtain");
 
 hamburgerIcon.addEventListener("click",(menu)=>{
     toggleMobileMenu(mobileNavMenu);
@@ -10,10 +11,16 @@ hamburgerIcon.addEventListener("click",(menu)=>{
 webPageBody.addEventListener("click",()=>{
     if(mobileNavMenu.classList.contains('open')){
         mobileNavMenu.classList.remove('open');
+        navCurtain.style.visibility = "hidden";
     }
 });
 
 
 function toggleMobileMenu(menu){
     menu.classList.toggle('open');
+    if(mobileNavMenu.classList.contains('open')){
+        navCurtain.style.visibility = "visible";
+    }else{
+        navCurtain.style.visibility = "hidden";
+    }
 }
