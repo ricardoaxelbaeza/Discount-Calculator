dfc0b2662521ec679666130f569d54fb98e193e0
ricardoaxelbaeza <ricardobaeza2000@gmail.com>
2024-08-07 21:39:23 -0700

    scrolling issues fixed on mobile menu

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -2,6 +2,7 @@ const hamburgerIcon = document.getElementById("hamburger-icon");
 const webPageBody = document.getElementById("calculator");
 const mobileNavMenu = document.getElementById("mobile-nav");
 const navCurtain = document.getElementById("nav-curtain");
+const body = document.getElementById("body");
 
 hamburgerIcon.addEventListener("click",(menu)=>{
     toggleMobileMenu(mobileNavMenu);
@@ -12,6 +13,7 @@ webPageBody.addEventListener("click",()=>{
     if(mobileNavMenu.classList.contains('open')){
         mobileNavMenu.classList.remove('open');
         navCurtain.style.visibility = "hidden";
+        body.style.overflow = "visible"
     }
 });
 
@@ -20,7 +22,9 @@ function toggleMobileMenu(menu){
     menu.classList.toggle('open');
     if(mobileNavMenu.classList.contains('open')){
         navCurtain.style.visibility = "visible";
+        body.style.overflow = "hidden"
     }else{
         navCurtain.style.visibility = "hidden";
+        body.style.overflow = "visible"
     }
 }
